aa353ad4724dcee1ed7a151479fb896aafad2d19
LeeGwonChang <kkd2014@gmail.com>
2023-07-19 17:25:44 +0900

    0719 - JWT 토큰 로그인   - 회원번호 인증 후 토큰 생성   - 회원번호 인증 후 토큰이 유효하다면 토큰 검증 - 서버 세션에 토큰 저장 - 토큰 유효 시 자동 로그인

--- lib/db.dart
+++ lib/db.dart
@@ -4,7 +4,7 @@ class DatabaseProvider {
   static Future<PostgreSQLConnection> getConnection() async {
     final connection = PostgreSQLConnection(
       '192.168.30.8',
-      5432, // PostgreSQL 포트 번호
+       5432, // PostgreSQL 포트 번호
       'data',
       username: 'dbtech',
       password: 'dbtechpwd',
